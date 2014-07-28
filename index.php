24341ebff1667b4eeaeafd5f8fcebefe8dcbb322
magdesign <mag@magdesign.ch>
2014-07-28 09:59:00 +0200

    Adding Firmware upgrade feauture
    added function to upgrade firmware and upgrade controlpanel

--- index.php
+++ index.php
@@ -136,6 +136,19 @@ if ($_GET['action'] == 'screenoff') {
 	system("sudo /opt/vc/bin/tvservice -o");
 }
 
+if ($_GET['action'] == 'firmware') {
+	echo "upgrade player and sync";
+	system("sudo cp /media/usb/omxplayer /usr/bin/omxplayer");
+	system("sudo cp /media/usb/omxplayer.bin /usr/bin/omxplayer.bin");
+	system("sudo cp /media/usb/omxplayer-sync /usr/bin/omxplayer-sync");
+	
+}
+
+if ($_GET['action'] == 'controlpanel') {
+	echo "update controlpanel";
+	system("sudo cp -r /media/usb/www/* /var/www");
+	
+}
 
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -193,126 +206,134 @@ function MM_preloadImages() { //v3.0
 <p class="header_02"><span class="header_02top">_____________________________________________</span></p>
 <p class="header_02"><span class="header_02top"><span class="description">status messages above this line in orange</span></span></p>
 <p class="header_02"><span class="header_02top">***************************************************************</span></p>
-<p class="header_02"><span class="header_02top">PocketVJ Control Panel </span><span class="header_02top">v0.12 ¬©2014 </span></p>
+<p class="header_02"><span class="header_02top">PocketVJ Control Panel </span><span class="header_02top">v0.13 ©2014 </span></p>
 <p class="header_02"><span class="header_02top">by magdesign.ch</span></p>
 <p class="header_02"><span class="header_02top">***************************************************************</span></p>
 <table width="380" border="0" align="center" cellspacing="4">
   <tr>
     <td width="190" height="40"><a href="?action=stop"><img src="pics/stop.png" width="190" height="40" alt="STOP" /></a></td>
-    <td width="190" height="40" class="description">stop the player before doing any other action</td>
+    <td width="190" height="40" class="description"><p align="left">stop the player before doing any other action</p></td>
   </tr>
   <tr>
     <td height="40"><a href="?action=startmaster"><img src="pics/start.png" width="190" height="40" alt="START" /></a></td>
-    <td width="190" height="40" class="description">may take a while, dont hit it more than ones!</td>
+    <td width="190" height="40" class="description"><div align="left">may take a while, dont hit it more than ones!</div></td>
   </tr>
   <tr>
     <td width="190" height="40"><a href="?action=startslave"><img src="pics/start_slave.png" width="190" height="40" alt="START_slave" /></a></td>
-    <td width="190" height="40" class="description">may take a while, dont hit it more than ones!</td>
+    <td width="190" height="40" class="description"><div align="left">may take a while, dont hit it more than ones!</div></td>
   </tr>
   <tr>
     <td height="40"><a href="?action=startusb"><img src="pics/start_usb.png" width="190" height="40" alt="START_usb" /></a></td>
-    <td width="190" height="40" class="description">may take a while, dont hit it more than ones!</td>
+    <td width="190" height="40" class="description"><div align="left">may take a while, dont hit it more than ones!</div></td>
   </tr>
   <tr>
     <td height="40"><a href="?action=stopimage"><img src="pics/stopimage.png" width="190" height="40" alt="stop image viewer" /></a></td>
-    <td height="40" class="description">stops the image viewer</td>
+    <td height="40" class="description"><div align="left">stops the image viewer</div></td>
   </tr>
   <tr>
     <td height="40"><a href="?action=image"><img src="pics/start_image.png" width="190" height="40" alt="imageplayer" /></a></td>
-    <td height="40" class="description">starts the image viewer (plays all image files from internal storage) within 5sec.</td>
+    <td height="40" class="description"><div align="left">starts the image viewer (plays all image files from internal storage) within 5sec.</div></td>
   </tr>
   <tr>
     <td height="40"><a href="?action=imageusb"><img src="pics/start_imageusb.png" width="190" height="40" alt="imageplayer from usb" /></a></td>
-    <td height="40" class="description">starts the image viewer (plays all image files from usb storage) within 5sec.</td>
-  </tr>
-  <tr>
-    <td height="40"><a href="?action=startppt"><img src="pics/start_ppt.png" width="190" height="40" alt="start ppt" /></a></td>
-    <td height="40" class="description">start powerpoint</td>
+    <td height="40" class="description"><div align="left">starts the image viewer (plays all image files from usb storage) within 5sec.</div></td>
   </tr>
 </table>
 <p>&nbsp;</p>
 <table width="380" border="0" align="center" cellspacing="4">
   <tr>
     <td width="190" height="40"><a href="/eXtplorer/index.php" target="new"><img src="pics/open.png" width="190" height="40" alt="OPEN_filebrowser" /></a></td>
-    <td width="190" height="40" class="description">open filebrowser in a new tab</td>
+    <td width="190" height="40" class="description"><p align="left">open filebrowser in a new tab</p></td>
   </tr>
   <tr>
     <td width="190" height="40"><a href="?action=clean"><img src="pics/clean.png" width="190" height="40" alt="CLEAN" /></a></td>
-    <td width="190" height="40" class="description">remove unwanted hidden files</td>
+    <td width="190" height="40" class="description"><p align="left">remove unwanted hidden files</p></td>
   </tr>
   <tr>
     <td width="190" height="40"><a href="?action=eject"><img src="pics/eject.png" width="190" height="40" alt="EJECT" /></a></td>
-    <td width="190" height="40" class="description">eject the usb stick, if you forget it will not be readable on your computer!</td>
+    <td width="190" height="40" class="description"><p align="left">eject the usb stick, if you forget it will not be readable on your computer!</p></td>
   </tr>
   <tr>
     <td width="190" height="40"><a href="?action=mount"><img src="pics/mount.png" width="190" height="40" alt="MOUNT" /></a></td>
-    <td width="190" height="40" class="description"><p>mount usb stick</p></td>
+    <td width="190" height="40" class="description"><p align="left">mount usb stick</p></td>
   </tr>
   <tr>
     <td width="190" height="40"><a href="?action=reboot"><img src="pics/reboot.png" width="190" height="40" alt="REBOOT" /></a></td>
-    <td width="190" height="40" class="description">reboot</td>
+    <td width="190" height="40" class="description"><p align="left">reboot</p></td>
   </tr>
 </table>
 <p>Settings for Display:</p>
 <table width="380" border="0" align="center" cellspacing="4">
   <tr>
     <td height="40"><a href="?action=getresolution"><img src="pics/resolution.png" width="190" height="40" alt="RESOLUTION" /></a></td>
-    <td width="190" height="40" class="description">get the actual resolution</td>
+    <td width="190" height="40" class="description"><p align="left">get the actual resolution</p></td>
   </tr>
   <tr>
     <td width="190" height="40"><a href="?action=screenon"><img src="pics/screen_on.png" width="190" height="40" alt="screen on" /></a></td>
-    <td width="190" height="40" class="description">wake up connected screen with preferred settings (CEC)</td>
+    <td width="190" height="40" class="description"><p align="left">wake up connected screen with preferred settings (CEC)</p></td>
   </tr>
   <tr>
     <td width="190" height="40"><a href="?action=screenoff"><img src="pics/screen_off.png" width="190" height="40" alt="screen off" /></a></td>
-    <td width="190" height="40" class="description"><p>put the connected screen to power sleep (CEC)</p></td>
+    <td width="190" height="40" class="description"><p align="left">put the connected screen to power sleep (CEC)</p></td>
   </tr>
 </table>
 <p>Settings for Boot/Autostart:</p>
 <table width="380" border="0" align="center" cellspacing="4">
   <tr>
     <td width="190" height="40"><a href="?action=master"><img src="pics/setmaster.png" width="190" height="40" alt="MASTER" /></a></td>
-    <td width="190" height="40" class="description">set to autostart as master</td>
+    <td width="190" height="40" class="description"><p align="left">set to autostart as master</p></td>
   </tr>
   <tr>
     <td width="190" height="40"><a href="?action=slave"><img src="pics/setslave.png" width="190" height="40" alt="SLAVE" /></a></td>
-    <td width="190" height="40" class="description">set to autostart as slave</td>
+    <td width="190" height="40" class="description"><p align="left">set to autostart as slave</p></td>
   </tr>
   <tr>
     <td height="40"><a href="?action=extension1"><img src="pics/extension1.png" width="190" height="40" alt="EXT1" /></a></td>
-    <td width="190" height="40" class="description">set autostart to extension1</td>
+    <td width="190" height="40" class="description"><p align="left">set autostart to extension1</p></td>
   </tr>
   <tr>
     <td width="190" height="40"><a href="?action=usb"><img src="pics/setusb.png" width="190" height="40" alt="USB" /></a></td>
-    <td width="190" height="40" class="description">set autostart to usb disk mode, there must be a usb stick attached at boot!</td>
+    <td width="190" height="40" class="description"><p align="left">set autostart to usb disk mode, there must be a usb stick attached at boot!</p></td>
   </tr>
   <tr>
     <td height="40"><a href="?action=setimage"><img src="pics/setimage.png" width="190" height="40" alt="set image player" /></a></td>
-    <td height="40" class="description">set autostart to image viewer, plays all .jpg images from intern memory</td>
+    <td height="40" class="description"><p align="left">set autostart to image viewer, plays all .jpg images from intern memory</p></td>
   </tr>
   <tr>
     <td height="40"><a href="?action=setimageusb"><img src="pics/setimageusb.png" width="190" height="40" alt="set usb image player" /></a></td>
-    <td height="40" class="description">set autostart to image viewer from usb, plays all .jpg images from usb(stick must be present on boot!)</td>
+    <td height="40" class="description"><p align="left">set autostart to image viewer from usb, plays all .jpg images from usb(stick must be present on boot!)</p></td>
   </tr>
   <tr>
     <td height="40"><a href="?action=bootconf"><img src="pics/bootconf.png" width="190" height="40" alt="boot conf custom" /></a></td>
-    <td height="40" class="description">copy custom conf.txt from internal to bootloader</td>
+    <td height="40" class="description"><p align="left">copy custom conf.txt from internal to bootloader</p></td>
   </tr>
   <tr>
     <td width="190" height="40"><a href="?action=hdmi1"><img src="pics/hdmi1.png" width="190" height="40" alt="HDMI1" /></a></td>
-    <td width="190" height="40" class="description"><p>set autostart to force HDMI to VGA</p></td>
+    <td width="190" height="40" class="description"><p align="left">set autostart to force HDMI to VGA</p></td>
   </tr>
   <tr>
     <td width="190" height="40"><a href="?action=hdmi4"><img src="pics/hdmi4.png" width="190" height="40" alt="HDMI4" /></a></td>
-    <td width="190" height="40" class="description">set autostart to force HDMI 720p</td>
+    <td width="190" height="40" class="description"><p align="left">set autostart to force HDMI 720p</p></td>
   </tr>
   <tr>
     <td height="40"><a href="?action=forcevga"><img src="pics/forcevga.png" width="190" height="40" alt="force vga" /></a></td>
-    <td height="40" class="description">set autostart to use HDMI to VGA converter with a resolution of 800x600</td>
+    <td height="40" class="description"><p align="left">set autostart to use HDMI to VGA converter with a resolution of 800x600</p></td>
   </tr>
 </table>
+<p>Settings for Firmware/System:</p>
+<table width="380" border="0" align="center" cellspacing="4">
+  <tr>
+    <td width="190" height="40"><a href="?action=firmware"><img src="pics/firmware_update.png" width="190" height="40" alt="MASTER" /></a></td>
+    <td width="190" height="40" class="description"><p align="left">Upgrade to new Firmware from USB Stick</p></td>
+  </tr>
+  <tr>
+    <td height="40"><a href="?action=controlpanel"><img src="pics/cp_upgrade.png" width="190" height="40" alt="update" /></a></td>
+    <td height="40" class="description"><p align="left">Update to new ControlPanel from USB</p></td>
+  </tr>
+</table>
+<p>&nbsp;</p>
 <p><span class="header_02top">***************************************************************</span></p>
-<p class="header_02">¬© PocketVJ.com</p>
+<p class="header_02">© PocketVJ.com</p>
 <p>&nbsp;</p>
 <p>&nbsp;</p>
 <p>&nbsp;</p>
