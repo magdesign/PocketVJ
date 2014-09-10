190c1d49e3ba49243c9b44f4793b7e16237ba087
Simon Josi <me@yokto.net>
2014-09-10 16:31:35 +0200

    add volume_up and volume_down actions

--- index.php
+++ index.php
@@ -164,6 +164,14 @@ if ($_GET['action'] == 'controlpanel') {
 	system("sudo cp -r /media/usb/www/* /var/www");
 }
 
+if ($_GET['action'] == 'volume_up') {
+	system("sudo su - pi -c 'amixer set Master 10%+'");
+}
+
+if ($_GET['action'] == 'volume_down') {
+	system("sudo su - pi -c 'amixer set Master 10%-'");
+}
+
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
