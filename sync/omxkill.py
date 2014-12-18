c143dab27090ca0782681477f2d3b400ff809bae
magdesign <mag@magdesign.ch>
2014-12-18 18:30:21 +0100

    fixed error
    fixed the button script
    fixed the omxkill
    integrated screenshre

--- sync/omxkill.py
+++ sync/omxkill.py
@@ -1,2 +1,5 @@
 #!/bin/bash
-sudo pkill -9 -f omxplayer-sync; sudo pkill -9 -f omxplayer; sudo pkill -9 -f omxplayer.bin
+exec > /dev/null 2>&1
+sudo pkill -9 -f omxplayer-sync
+sudo pkill -9 -f omxplayer
+sudo pkill -9 -f omxplayer.bin
--- sync/pocketvj_final.py
+++ sync/pocketvj_final.py
@@ -17,47 +17,46 @@ GPIO.setup (22,GPIO.IN)
 
 # Button Klasse
 class Button(object):
-    def __init__(self, gpio_input, start_command = False, stop_command = False, callback = False):
+    def __init__(self, controller, gpio_input, start_command, stop_command):
+        self.controller = controller
         self.gpio_input = gpio_input
         self.input_buffer = 0
         self.input_buffer_prev = 0
         self.state = False
         self.start_command = start_command
         self.stop_command = stop_command
-        self.callback = callback
         
     def update_state(self):
         self.input_buffer = GPIO.input(self.gpio_input) # Hardware Button lesen
         if ((self.input_buffer_prev == 0) and (self.input_buffer == 1)): # button press (entprellt)
-            if callable(self.callback):
-                self.callback(self.gpio_input)
-                
             if self.state:
                if self.stop_command: self.stop_command() 
             else:
-               if self.start_command: self.start_command()
-                
+                if self.controller.last_button and self.controller.last_button != self.gpio_input:
+                    last_button = self.controller.buttons[self.controller.last_button]
+                    last_button.stop_command()
+                if self.start_command: self.start_command()
+            
             self.state = not self.state
-            print "%d: %s" % (self.gpio_input, self.state)
+            self.controller.last_button = self.gpio_input
+            for button in self.controller.buttons.values():
+                if button.gpio_input != self.gpio_input:
+                    button.state = False
+
         self.input_buffer_prev = self.input_buffer # Entprellung
 
 # Buttons Klasse
-class Buttons(object):
+class ButtonController(object):
     def __init__(self, button_data):
-        self.buttons = []
+        self.buttons = {}
+        self.last_button = None
+
         for button in button_data:
-            self.buttons.append(Button(button[0], button[1], button[2], self.clear_buttons))
+            self.buttons[button[0]] = Button(self, button[0], button[1], button[2])
 
         while(True):
-            for button in self.buttons:
+            for button in self.buttons.values():
                 button.update_state()
-            
-    def clear_buttons(self, gpio_input):
-        for button in self.buttons:
-            if button.gpio_input != gpio_input:
-                button.state = False
-                if button.stop_command: button.stop_command()
-        
 
 # Commands
 def cmd_stop_omx():     #define stop kommando
@@ -75,7 +74,6 @@ def cmd_start_vid2():   #define video 2
 def cmd_start_vid3():   #define video 3
     os.system ("/usr/bin/omxplayer-sync -mu /media/internal/03* &")
     print "start movie 03"
-
     
 def cmd_start_vid4():   #define video 4
     os.system ("/usr/bin/omxplayer-sync -mu /media/internal/04* &")
@@ -96,8 +94,8 @@ def cmd_start_vid7():   #define video 7
 def cmd_start_vid8():   #define video 8
     os.system ("/usr/bin/omxplayer-sync -mu /media/internal/08* &")
     print "start movie 08"
-    
-Buttons([
+
+ButtonController([
   (13, cmd_start_vid1, cmd_stop_omx),
   (12, cmd_start_vid2, cmd_stop_omx),
   (11, cmd_start_vid3, cmd_stop_omx),
@@ -107,7 +105,6 @@ Buttons([
   (16, cmd_start_vid7, cmd_stop_omx),
   (15, cmd_start_vid8, cmd_stop_omx),
 ])
-
 
 # cleanup
 GPIO.cleanup()
